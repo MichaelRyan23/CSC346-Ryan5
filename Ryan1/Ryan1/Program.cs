ccc862ce7dff44f2fd5c35edc5a7b3a56ebfaf93
MichaelRyan23 <michaelryan1823@gmail.com>
2024-01-17 13:53:43 -0600

    added gitignore file

--- Ryan1/Ryan1/Program.cs
+++ Ryan1/Ryan1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Michael Ryan\nCSC346\nAssignment 1\nDue Date: 1/24/2023");
  
